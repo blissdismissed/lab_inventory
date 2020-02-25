b6d21a9341e94295aac1726953a352768f3d60fd
ajautz@gmail.com <ajautz@gmail.com>
2020-02-24 20:06:58 -0500

    Modify Items table

---
+++ db/migrate/20200225003355_change_items.rb
@@ -0,0 +1,7 @@
+class ChangeItems < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :items, :quantity
+    remove_column :items, :name
+    add_column :items, :item_type_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_234405) do
+ActiveRecord::Schema.define(version: 2020_02_25_003355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,11 @@ ActiveRecord::Schema.define(version: 2020_02_24_234405) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "quantity", null: false
     t.string "room_location", null: false
     t.string "where_in_room", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "item_type_id"
   end
 
 end
